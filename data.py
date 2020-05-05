18ad9d352337fa7f0fe009e44b5575482b10b123
Luckysting <work.romanuel@gmail.com>
2020-05-05 19:45:23 +0300

    Add horizontal flip of images

--- data.py
+++ data.py
@@ -1,46 +1,32 @@
+from random import Random
 import numpy as np
 import skimage.io as io
 import skimage.transform as trans
 import os
 
-from keras.preprocessing.image import ImageDataGenerator
-
 
 def format_image(image):
-    img = image / 255
-    img = trans.resize(img, (256, 256))
-    img = np.reshape(img, img.shape + (1,))
-    return np.reshape(img, (1,) + img.shape)
+    image = image / 255
+    image = trans.resize(image, (256, 256))
+    image = np.reshape(image, image.shape + (1,))
+    image = np.reshape(image, (1,) + image.shape)
+    return image
 
 
-def normalize_image(image):
-    img = image[0, :, :, 0]
-    img /= 255
-    img = np.reshape(img, img.shape + (1,))
-    return np.reshape(img, (1,) + img.shape)
-
-
-def aug_data_generator(path):
-    x_gen = ImageDataGenerator(horizontal_flip=True)
-    y_gen = ImageDataGenerator(horizontal_flip=True)
-
-    x_images = x_gen.flow_from_directory(path, (256, 256), 'grayscale', classes=['x'], batch_size=1, seed=1)
-    y_images = y_gen.flow_from_directory(path, (256, 256), 'grayscale', classes=['y'], batch_size=1, seed=1)
-
-    train_generator = zip(x_images, y_images)
-
-    for (img_x, img_y) in train_generator:
-        yield normalize_image(img_x[0]), normalize_image(img_y[0])
-
-
-def data_generator(path, start, length):
+def data_generator(path, start, length, flip=False):
     i = 0
+    random = Random()
     while True:
         ind = start + (i % length)
-        img_x = format_image(io.imread(os.path.join(path, 'x', 'out_{}.png'.format(ind)), as_gray=True))
-        img_y = format_image(io.imread(os.path.join(path, 'y', 'in_{}.png'.format(ind)), as_gray=True))
+        img_x = io.imread(os.path.join(path, 'x', 'out_{}.png'.format(ind)), as_gray=True)
+        img_y = io.imread(os.path.join(path, 'y', 'in_{}.png'.format(ind)), as_gray=True)
+        if flip and random.randint(1, 2) == 1:
+            img_x = np.flip(img_x, axis=1)
+            img_y = np.flip(img_y, axis=1)
+        img_x = format_image(img_x)
+        img_y = format_image(img_y)
         i += 1
-        yield format_image(img_x[0]), format_image(img_y[0])
+        yield img_x, img_y
 
 
 def save_result(path, result, start=1):
@@ -50,4 +36,4 @@ def save_result(path, result, start=1):
         max_v = np.max(img)
         img /= max_v
         img *= 255
-        io.imsave(os.path.join(path, "predict_{}.png".format(start + i)), img)
+        io.imsave(os.path.join(path, 'predict_{}.png'.format(start + i)), img)
--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from keras.callbacks import ModelCheckpoint
 from data import data_generator, save_result
 from unet import unet
 
-train_generator = data_generator('training_set', 1, 15)
-test_generator = data_generator('training_set', 1, 1)
+train_generator = data_generator('training_set', 1, 200, flip=True)
+test_generator = data_generator('training_set', 205, 5, flip=True)
+
+weight_name = 'test'
 
 model = unet()
 
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss', verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint('{}.hdf5'.format(weight_name), monitor='loss', verbose=1, save_best_only=True)
 
-model.fit_generator(train_generator, steps_per_epoch=15, epochs=500, callbacks=[model_checkpoint])
-result = model.predict_generator(test_generator, steps=1, verbose=1)
-save_result('.', result)
+model.fit_generator(train_generator, steps_per_epoch=200, epochs=200, callbacks=[model_checkpoint])
+result = model.predict_generator(test_generator, steps=5, verbose=1)
+
+save_result('./{}'.format(weight_name), result, 205)
