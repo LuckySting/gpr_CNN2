77d2147483be2e0356a0cd352950ae827e5993df
Luckysting <work.romanuel@gmail.com>
2020-05-05 19:09:57 +0300

    Add augmentation

--- data.py
+++ data.py
@@ -7,23 +7,30 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 def format_image(image):
-    img_x = image / 255
-    img_x = trans.resize(img_x, (256, 256))
-    img_x = np.reshape(img_x, img_x.shape + (1,))
-    return np.reshape(img_x, (1,) + img_x.shape)
+    img = image / 255
+    img = trans.resize(img, (256, 256))
+    img = np.reshape(img, img.shape + (1,))
+    return np.reshape(img, (1,) + img.shape)
+
+
+def normalize_image(image):
+    img = image[0, :, :, 0]
+    img /= 255
+    img = np.reshape(img, img.shape + (1,))
+    return np.reshape(img, (1,) + img.shape)
 
 
 def aug_data_generator(path):
     x_gen = ImageDataGenerator(horizontal_flip=True)
     y_gen = ImageDataGenerator(horizontal_flip=True)
 
-    x_images = x_gen.flow_from_directory(path + '/x', (256, 256), 'grayscale', seed=1)
-    y_images = y_gen.flow_from_directory(path + '/y', (256, 256), 'grayscale', seed=1)
+    x_images = x_gen.flow_from_directory(path, (256, 256), 'grayscale', classes=['x'], batch_size=1, seed=1)
+    y_images = y_gen.flow_from_directory(path, (256, 256), 'grayscale', classes=['y'], batch_size=1, seed=1)
 
     train_generator = zip(x_images, y_images)
 
     for (img_x, img_y) in train_generator:
-        yield format_image(img_x), format_image(img_y)
+        yield normalize_image(img_x[0]), normalize_image(img_y[0])
 
 
 def data_generator(path, start, length):
@@ -33,7 +40,7 @@ def data_generator(path, start, length):
         img_x = format_image(io.imread(os.path.join(path, 'x', 'out_{}.png'.format(ind)), as_gray=True))
         img_y = format_image(io.imread(os.path.join(path, 'y', 'in_{}.png'.format(ind)), as_gray=True))
         i += 1
-        yield img_x, img_y
+        yield format_image(img_x[0]), format_image(img_y[0])
 
 
 def save_result(path, result, start=1):
